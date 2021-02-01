5b11c47d00ba8bff344544680a7ff04a6a6940a7
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-02-01 19:14:38 +0900

    add async logic for get main coordinates

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+// 175.126.38.231
+import axios from 'axios';
+
+const fetchIP = async () => {
+  try {
+    const result = await axios.get('https://freegeoip.app/json/175.126.38.231');
+
+    return result.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export {
+  fetchIP,
+}
